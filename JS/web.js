336ce1b73df26b6d9c1d8fd73680c7f7dbc6d6b1
David Gaspar <davsangas@hotmail.com>
2022-12-28 21:56:02 -0600

    Starting API conection

---
+++ JS/web.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    //Header
+    let countryTitle = document.querySelector('#Country-title')
+
+    //Main
+    let statusLogo = document.querySelector('.status-logo')
+    let descriptionWeather = document.querySelector('.descriptionWeather')
+    let temperature = document.querySelector('.temperature')
+    let weatherData = document.querySelector('.data')
+
+    //Footer(CSS to check)
+})

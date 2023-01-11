b4e7de4d039ea4e3e7c09505af410965a5f94034
David Gaspar <davsangas@hotmail.com>
2023-01-10 22:24:36 -0600

    End of the structure for request weather forecast

--- JS/web.js
+++ JS/web.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('load', () => {
     //Header
     let countryTitle = document.querySelector('.Country-title')
@@ -13,19 +12,32 @@ window.addEventListener('load', () => {
     let HumidityData = document.querySelector('.Humidity')
     let UVData = document.querySelector('.UV')
 
-    //Footer(CSS to check)
+    //Footer
+
+    let futureTemperatureSection = document.querySelector('#Future-temperature-section')
+    let tempSection = document.querySelector('.temperature-section')
+    let logo = document.querySelector('.logo')
+    let tempDay = document.querySelector('.temperature-day')
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             lon = position.coords.longitude
             lat = position.coords.latitude
             const api_key = API_KEY
-            const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${api_key}`
+            const baseURL = 'https://api.weatherbit.io/v2.0/'
+            const days = 7
 
-            axios
-                .get(url)
-                .then(Response => {
-                    const data = Response.data
+            const axiosInstance1 = axios.create({
+                baseURL: baseURL
+            })
+            axiosInstance1.get('/current', {
+                params: {
+                    lat: lat,
+                    lon: lon,
+                    key: api_key
+                }
+            }).then(Response => {
+                const data = Response.data
                     const cityName = data.data[0].city_name
                     countryTitle.textContent = cityName
                     console.log(data)
@@ -52,7 +64,7 @@ window.addEventListener('load', () => {
                     const temp = Math.round(data.data[0].temp)
                     temperature.textContent = `${temp}°C`
 
-                    //Data Generals
+                    //Generals-data
                     const wind = data.data[0].wind_spd
                     windData.textContent = `Velocidad del viento: ${wind}m/s`
 
@@ -67,7 +79,20 @@ window.addEventListener('load', () => {
                     } else if (UV <= 80) {
                         UVData.textContent = 'Índice UV: Alto'
                     }
-                })
+            })
+            const axiosInstance2 = axios.create({
+                baseURL: baseURL
+            })
+            axiosInstance2.get(`/forecast/daily`, {
+                params: {
+                    lat: lat,
+                    lon: lon,
+                    days: days,
+                    key: api_key,
+                }
+            }).then(Response => {
+                console.log(Response.data)
+            })
         })
     }
 })

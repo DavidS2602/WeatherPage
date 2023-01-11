bd69ae13d955427ddde83112114dfa2ee8d00802
David Gaspar <davsangas@hotmail.com>
2023-01-11 09:27:12 -0600

    Conclusion of the request, animation pending

--- JS/web.js
+++ JS/web.js
@@ -14,10 +14,27 @@ window.addEventListener('load', () => {
 
     //Footer
 
-    let futureTemperatureSection = document.querySelector('#Future-temperature-section')
-    let tempSection = document.querySelector('.temperature-section')
+    let day0 = document.querySelector('.day0')
+    let day1 = document.querySelector('.day1')
+    let day2 = document.querySelector('.day2')
+    let day3 = document.querySelector('.day3')
+    let day4 = document.querySelector('.day4')
     let logo = document.querySelector('.logo')
-    let tempDay = document.querySelector('.temperature-day')
+    let tempMax0 = document.querySelector('.temp-max0')
+    let tempMin0 = document.querySelector('.temp-min0')
+
+    let tempMax1 = document.querySelector('.temp-max1')
+    let tempMin1 = document.querySelector('.temp-min1')
+
+    let tempMax2 = document.querySelector('.temp-max2')
+    let tempMin2 = document.querySelector('.temp-min2')
+
+    let tempMax3 = document.querySelector('.temp-max3')
+    let tempMin3 = document.querySelector('.temp-min3')
+
+    let tempMax4 = document.querySelector('.temp-max4')
+    let tempMin4 = document.querySelector('.temp-min4')
+
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -40,7 +57,6 @@ window.addEventListener('load', () => {
                 const data = Response.data
                     const cityName = data.data[0].city_name
                     countryTitle.textContent = cityName
-                    console.log(data)
 
                     const Clouds = data.data[0].clouds
                     const humidity = data.data[0].rh
@@ -80,6 +96,18 @@ window.addEventListener('load', () => {
                         UVData.textContent = 'Índice UV: Alto'
                     }
             })
+
+            let dayName= ''//Store the days for each iteration
+            const daysOfWeek = [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday',
+            ]
+
             const axiosInstance2 = axios.create({
                 baseURL: baseURL
             })
@@ -91,8 +119,75 @@ window.addEventListener('load', () => {
                     key: api_key,
                 }
             }).then(Response => {
+                const data = Response.data
+                data.data.forEach(dayData => {
+                    const datetime = dayData.datetime;
+                    const dayDate = new Date(datetime);
+                    const dayOfWeek = dayDate.getUTCDay();
+                    dayName = daysOfWeek[dayOfWeek];
+                });
                 console.log(Response.data)
+                /*--------------------Description 0--------------------*/
+                dayName = data.data[0].datetime
+                day0.textContent = dayName
+                console.log(dayName)
+
+                /*--------------------Temp max-min--------------------*/
+                const MaxTemp0 = data.data[0].max_temp
+                tempMax0.textContent = `Temp.Max: ${MaxTemp0}°C`
+
+                const MinTemp0 = data.data[0].min_temp
+                tempMin0.textContent = `Temp.Min: ${MinTemp0}°C`
+
+                /*--------------------Description 1--------------------*/
+                dayName = data.data[1].datetime
+                day1.textContent = dayName
+                console.log(dayName)
+
+                /*--------------------Temp max-min--------------------*/
+                const MaxTemp1 = data.data[1].max_temp
+                tempMax1.textContent = `Temp.Max: ${MaxTemp1}°C`
+
+                const MinTemp1 = data.data[1].min_temp
+                tempMin1.textContent = `Temp.Min: ${MinTemp1}°C`
+
+                /*--------------------Description 2--------------------*/
+                dayName = data.data[2].datetime
+                console.log(dayName)
+                day2.textContent = dayName
+
+                /*--------------------Temp max-min--------------------*/
+                const MaxTemp2 = data.data[2].max_temp
+                tempMax2.textContent = `Temp.Max: ${MaxTemp2}°C`
+
+                const MinTemp2 = data.data[2].min_temp
+                tempMin2.textContent = `Temp.Min: ${MinTemp2}°C`
+
+                /*--------------------Description 3--------------------*/
+                dayName = data.data[3].datetime
+                console.log(dayName)
+                day3.textContent = dayName
+
+                /*--------------------Temp max-min--------------------*/
+                const MaxTemp3 = data.data[3].max_temp
+                tempMax3.textContent = `Temp.Max: ${MaxTemp3}°C`
+
+                const MinTemp3 = data.data[3].min_temp
+                tempMin3.textContent = `Temp.Min: ${MinTemp3}°C`
+
+                /*--------------------Description 3--------------------*/
+                dayName = data.data[4].datetime
+                console.log(dayName)
+                day4.textContent = dayName
+
+                /*--------------------Temp max-min--------------------*/
+                const MaxTemp4 = data.data[4].max_temp
+                tempMax4.textContent = `Temp.Max: ${MaxTemp4}°C`
+
+                const MinTemp4 = data.data[4].min_temp
+                tempMin4.textContent = `Temp.Min: ${MinTemp4}°C`
             })
         })
     }
 })
+
